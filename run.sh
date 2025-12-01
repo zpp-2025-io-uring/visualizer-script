7257fb54b303647b85da60774eb212b14ae31bb6
MrD4rkne <marcinsz.pv@gmail.com>
2025-12-01 18:09:52 +0100

    Fix quoting issues and typos in run script

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-IO_TESTER=$1
-PLOT_NUM_SHARDS=$2
+IO_TESTER="$(realpath $1)"
+PLOT_NUM_SHARDS="$2"
 
 mkdir -p build
 cd build
 
 mkdir -p temp
 
-if $IO_TESTER --conf ../config.yaml --storage './temp' > asynchronous.in; then
-    echo "Successfully ran asynchronous banchmark"
+if "$IO_TESTER" --conf ../config.yaml --storage './temp' > asynchronous.in; then
+    echo "Successfully ran asynchronous benchmark"
 else
     echo "Asynchronous benchmark failed"
 fi
 
-if $IO_TESTER --conf ../config.yaml --storage './temp' --reactor-backend io_uring > synchronous.in; then
-    echo "Successfully ran synchronous banchmark"
+if "$IO_TESTER" --conf ../config.yaml --storage './temp' --reactor-backend io_uring > synchronous.in; then
+    echo "Successfully ran synchronous benchmark"
 else
     echo "Synchronous benchmark failed"
 fi
 
 rmdir temp
 
-python3 ../main.py $PLOT_NUM_SHARDS
+python3 ../main.py "$PLOT_NUM_SHARDS"
