d26dacdda429c6dbc1adaffbb8e9474b02fff9d2
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2026-01-11 19:35:54 +0100

    Update run io to take a configuration dict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-from run_io import configure_run_io_parser
 from run_rpc import configure_run_rpc_parser
 from redraw import configure_redraw_parser
 from benchmarks import configure_run_benchmark_suite_parser
@@ -10,7 +9,6 @@ if __name__ == "__main__":
 
     subparsers = parser.add_subparsers(dest="subprogram", required=True, description="subprogram to execute")
 
-    configure_run_io_parser(subparsers.add_parser(name="io", help="run io_tester"))
     configure_run_rpc_parser(subparsers.add_parser(name="rpc", help="run rpc_tester"))
     configure_redraw_parser(subparsers.add_parser(name="redraw", help="generate graphs from existing inputs"))
     configure_redraw_suite_parser(subparsers.add_parser(name="redraw_suite", help="generate graphs for an existing run"))
