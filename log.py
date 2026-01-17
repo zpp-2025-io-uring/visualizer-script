281b8ab4933cb724f814a8eeec5c8a9cf93ae330
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2026-01-17 19:55:39 +0100

    Add log level selection

--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 import logging
 from colorama import Fore
 
+_level = logging.NOTSET
 
 class ColoredLogger(logging.Formatter):
     def format(self, record):
@@ -19,11 +20,25 @@ class ColoredLogger(logging.Formatter):
         message = super().format(record)
         return f"{color}{message}{Fore.RESET}"
 
+def set_level(new_level: str):
+    global _level
+    match new_level:
+        case "debug":
+            _level = logging.DEBUG
+        case "info":
+            _level = logging.INFO
+        case "warning":
+            _level = logging.WARNING
+        case "error":
+            _level = logging.ERROR
+        case "critical":
+            _level = logging.CRITICAL
 
-def get_logger(name) -> logging.Logger:
+
+def get_logger(name: str | None) -> logging.Logger:
     handler = logging.StreamHandler()
     handler.setFormatter(ColoredLogger("%(levelname)s: %(message)s"))
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(_level)
     logger.addHandler(handler)
     return logger
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 
 from benchmarks import configure_run_benchmark_suite_parser
+from log import set_level
 from redraw import configure_redraw_parser
 from redraw_suite import configure_redraw_suite_parser
 
@@ -15,6 +16,9 @@ if __name__ == "__main__":
     )
     configure_run_benchmark_suite_parser(subparsers.add_parser(name="suite", help="run a benchmark suite"))
 
+    parser.add_argument("--log-level", help="logger output level", choices=["debug", "info", "warning", "error", "critical"], default="info")
+
     args = parser.parse_args()
+    set_level(args.log_level)
 
     args.func(args)
