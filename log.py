e71fa863ea54053090d8fe168010b03da7124d7b
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2026-01-17 19:20:58 +0100

    Added colors

--- log.py
+++ log.py
@@ -1,10 +1,22 @@
 import logging
-
+from colorama import Fore
 
 class ColoredLogger(logging.Formatter):
     def format(self, record):
+        match record.levelno:
+            case logging.DEBUG:
+                color = Fore.WHITE
+            case logging.INFO:
+                color = Fore.BLUE
+            case logging.WARNING:
+                color = Fore.YELLOW
+            case logging.ERROR:
+                color = Fore.RED
+            case logging.CRITICAL:
+                color = Fore.RED
+
         message = super().format(record)
-        return f"{message}"
+        return f"{color}{message}{Fore.RESET}"
 
 
 def get_logger(name) -> logging.Logger:
