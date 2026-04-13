abbd6bec416d0dfed94a4065773f8be4a9435bd1
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2026-04-13 11:23:26 +0200

    Apply ruff format to remote.py

--- remote.py
+++ remote.py
@@ -6,6 +6,7 @@ from log import get_logger
 
 logger = get_logger()
 
+
 @dataclass
 class CmdOutput:
     stdout: str
@@ -15,13 +16,14 @@ class CmdOutput:
     def from_json(data: dict) -> "CmdOutput":
         return CmdOutput(stdout=data["stdout"], stderr=data["stderr"], returncode=data["return_code"])
 
+
 class RemoteProcess:
     def __init__(self, remote: "Remote", pid: int):
         self.remote = remote
         self.pid = pid
-    
+
     def wait(self) -> CmdOutput:
-        response =  requests.post(f"http://{self.remote.address}/wait_and_output", json=self.pid)
+        response = requests.post(f"http://{self.remote.address}/wait_and_output", json=self.pid)
         print(response.text)
         if response.ok:
             return CmdOutput.from_json(response.json())
@@ -29,55 +31,66 @@ class RemoteProcess:
             raise RuntimeError(f"Remote failed with response {response.status_code}")
 
     def kill(self):
-        response =  requests.post(f"http://{self.remote.address}/kill", json=self.pid)
+        response = requests.post(f"http://{self.remote.address}/kill", json=self.pid)
         if not response.ok:
             raise RuntimeError(f"Remote failed with response {response.status_code}")
-        
+
     def terminate(self):
-        response =  requests.post(f"http://{self.remote.address}/terminate", json=self.pid)
+        response = requests.post(f"http://{self.remote.address}/terminate", json=self.pid)
         if not response.ok:
             raise RuntimeError(f"Remote failed with response {response.status_code}")
-        
+
     def poll(self) -> int | None:
-        response =  requests.post(f"http://{self.remote.address}/poll", json=self.pid)
+        response = requests.post(f"http://{self.remote.address}/poll", json=self.pid)
         if response.ok:
             return response.json()
         else:
             raise RuntimeError(f"Remote failed with response {response.status_code}")
 
+
 class Remote:
     def __init__(self, address: str):
         self.address = address
 
-    def run_io_tester(self, config: str, backend: str, app_cpuset: str, async_worker_cpuset: str | None) -> RemoteProcess:
+    def run_io_tester(
+        self, config: str, backend: str, app_cpuset: str, async_worker_cpuset: str | None
+    ) -> RemoteProcess:
         params = {
-            "config":config,
-            "backend":backend,
-            "app_cpuset":app_cpuset,
-            "async_worker_cpuset":async_worker_cpuset
+            "config": config,
+            "backend": backend,
+            "app_cpuset": app_cpuset,
+            "async_worker_cpuset": async_worker_cpuset,
         }
 
-        response =  requests.post(f"http://{self.address}/io_tester", json=params)
+        response = requests.post(f"http://{self.address}/io_tester", json=params)
         if response.ok:
             return RemoteProcess(remote=self, pid=response.json())
         else:
             raise RuntimeError(f"Remote failed with response {response.status_code}")
 
-    def run_rpc_tester(self, config: str, backend: str, ip_address: str, port: str, is_server: bool, app_cpuset: str, async_worker_cpuset: str | None) -> RemoteProcess:
+    def run_rpc_tester(
+        self,
+        config: str,
+        backend: str,
+        ip_address: str,
+        port: str,
+        is_server: bool,
+        app_cpuset: str,
+        async_worker_cpuset: str | None,
+    ) -> RemoteProcess:
         params = {
-            "config":config,
-            "backend":backend,
-            "ip_address":ip_address,
-            "port":port,
-            "is_server":is_server,
-            "app_cpuset":app_cpuset,
-            "async_worker_cpuset":async_worker_cpuset
+            "config": config,
+            "backend": backend,
+            "ip_address": ip_address,
+            "port": port,
+            "is_server": is_server,
+            "app_cpuset": app_cpuset,
+            "async_worker_cpuset": async_worker_cpuset,
         }
 
-        response =  requests.post(f"http://{self.address}/rpc_tester", json=params)
+        response = requests.post(f"http://{self.address}/rpc_tester", json=params)
         if response.ok:
             return RemoteProcess(remote=self, pid=response.json())
         else:
             print(response.text)
             raise RuntimeError(f"Remote failed with response {response.status_code}")
-
